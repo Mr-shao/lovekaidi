f1e7dd13b0623038c4cba573b32840b861925b31
Stitch <m15604801235@163.com>
2019-04-06 23:39:53 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -53,6 +53,17 @@
 	var _ManagePageComponent = __webpack_require__(4);
 
 	var _ManagePageComponent2 = _interopRequireDefault(_ManagePageComponent);
+	var aud;
+	document.ontouchend = function () {
+		if (!aud) {
+			aud = new Audio('./src/7.mp3').play();
+		}
+	}
+	document.onclick = function () {
+		if (!aud) {
+			aud = new Audio('./src/7.mp3').play();
+		}
+	}
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
